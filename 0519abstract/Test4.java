ea862eb2e7ae60b6e711cb27d83c40d8b9b734ff
ChengyuanLi <ca4103@gmail.com>
2021-05-19 17:20:25 -0400

    class relationship

---
+++ 0519abstract/Test4.java
@@ -0,0 +1,25 @@
+public class Test4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Pig {
+
+}
+
+class Knife {
+
+}
+
+class Butcher {
+
+    public void sing() {
+
+    }
+
+    public void kill(Pig pig){
+        Pig p = new Pig();
+    }
+}
+
