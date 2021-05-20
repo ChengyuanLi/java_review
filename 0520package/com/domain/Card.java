98ecf8c0219d6fbe93d1230b307e6f5f53f490e6
Aiden Li <aidenli@AidendeMacBook-Pro.local>
2021-05-20 18:19:51 -0400

    包结构

---
+++ 0520package/com/domain/Card.java
@@ -0,0 +1,16 @@
+package com.domain;
+public class Card {
+	int cno;
+	int pass;
+	int balance;
+	String cname;
+
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+
+	public int getCno() {
+		return cno;
+
+	}
+}
---
+++ 0520package/com/test/TestMain.java
@@ -0,0 +1,8 @@
+package com.test;
+import  com.domain.Card;
+public class TestMain {
+	public static void main(String[] args) {
+		Card card = new Card();
+	}
+	
+}
