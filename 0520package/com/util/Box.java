640e606360f68a14d792b85297bcde95f701ddd8
ChengyuanLi <ca4103@gmail.com>
2021-05-20 20:29:23 -0400

    我也不知道干了啥

---
+++ 0520package/com/util/Box.java
@@ -0,0 +1,8 @@
+package com.util;
+public interface Box {
+    public boolean add(Object object);
+    public int size();
+    public Object get(int i);
+    public Object remove(int i);
+    public Object[] toArray();
+}
---
+++ 0520package/com/util/LinkedBox.java
@@ -0,0 +1,18 @@
+package com.util;
+public class LinkedBox implements Box{
+    public boolean add(Object object){
+        return false;
+    }
+    public int size(){
+        return 1;
+    }
+    public Object get(int i){
+        return null;
+    }
+    public Object remove(int i){
+        return null;
+    }
+    public Object[] toArray(){
+        return null;
+    }
+}
