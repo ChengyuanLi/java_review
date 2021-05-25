34e2c94b83bbe3e6910c584a10deaead67d41345
ChengyuanLi <ca4103@gmail.com>
2021-05-25 15:16:45 -0400

    双色球项目

--- 0524/src/com/test2/TestMain.java
+++ 0524/src/com/test2/TestMain.java
@@ -1,5 +1,6 @@
 package com.test2;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -16,10 +17,73 @@ import java.util.Scanner;
 public class TestMain {
     public static void main(String[] args) {
         int[] myNums = inputMyNum();
+        int[] gNums = generator();
+        int countR = 0;
+        boolean blue = false;
+
+        for (int i = 0; i < myNums.length - 1; i++) {
+            int myNum = myNums[i];
+            for (int j = 0; j < gNums.length - 1; j++) {
+                int gNum = gNums[j];
+                if (myNum == gNum) {
+                    countR++;
+                    break;
+                }
+            }
+        }
+        blue = myNums[myNums.length - 1] == gNums[gNums.length - 1];
+
+        sort(myNums);
+        sort(gNums);
+
+        for(int i = 0; i < myNums.length; i++) {
+            System.out.print(myNums[i] + "\t");
+        }
+        System.out.println();
+        for(int i = 0; i < myNums.length; i++) {
+            System.out.print(gNums[i] + "\t");
+        }
+        System.out.println();
+
+      if (countR == 6 && blue) {
+            System.out.println("500w到手！！！！！");
+        } else if (countR == 3 && blue) {
+            System.out.println("10块到手");
+        } else if (countR == 4 || blue) {
+            System.out.println("5块到手");
+        } else {
+            System.out.println("谢谢惠顾");
+        }
+
     }
+
+    public static void sort(int[] nums) {
+        for (int i = 1; i < nums.length - 1; i++) {
+            for (int j = 1; j <= nums.length - i - 1; j++) {
+                if (nums[j - 1] > nums[j]) {
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+
     public static int[] generator(){
+        Random random = new Random();
         int[] nums = new int[7];
         int count = 0;
+        a: while (count < 6) {
+            int num = random.nextInt(33) + 1;
+            for (int j = 0; j < count; j++) {
+                if (nums[j] == num) {
+                    continue a;
+                }
+            }
+            nums[count] = num;
+            count++;
+        }
+        nums[nums.length - 1] = random.nextInt(16) + 1;
         return nums;
     }
     public static int[] inputMyNum() {
