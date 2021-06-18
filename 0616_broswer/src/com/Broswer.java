61005b2285ed8c1319828c9ba0be08541f36ef37
ChengyuanLi <ca4103@gmail.com>
2021-06-18 14:36:22 -0400

    browser & server 3.0

--- 0616_broswer/src/com/Broswer.java
+++ 0616_broswer/src/com/Broswer.java
@@ -5,6 +5,9 @@ import com.util.Input;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author aidenli
@@ -15,6 +18,9 @@ public class Broswer {
 	// write your code here
         new Broswer().open();
     }
+
+    private String ip;
+    private int port;
 
     private void open() {
         System.out.print("URL: ");
@@ -34,10 +40,10 @@ public class Broswer {
         url = url.trim();
         int i1 = url.indexOf(":");
         String ipStr = url.substring(0, i1);
-
+        this.ip = ipStr;
         int i2 = url.indexOf("/");
         String portStr = url.substring(i1 + 1, i2);
-        int port = Integer.parseInt(portStr);
+        this.port = Integer.parseInt(portStr);
 
         String content = url.substring(i2); // with "/"
 
@@ -89,7 +95,70 @@ public class Broswer {
     }
 
     private void showContent(String content) {
-        System.out.println(content);
+        Map<String, String> params = new HashMap<>();
+        //from tag content
+        String requestContent = null;
+        int i = 0;
+        while (true) {
+            //start of block
+            int i1 = content.indexOf("[");
+            //end of block
+            int i2 = content.indexOf("]", i1);
+
+            if (i1 == -1 || i2 == -1) {
+                System.out.println(content);
+                break;
+            }
+
+            String before = content.substring(0, i1);
+            System.out.println(before);
+
+            String tag = content.substring(i1+1, i2);
+            if ("input".equals(tag) || tag.startsWith("input ")) {
+                String value = Input.next();
+                int i3 = tag.indexOf("<");
+                int i4 = tag.indexOf(">", i3);
+                if (i3 != -1 && i3 < i4) {
+                    String key = tag.substring(i3+1, i4);
+                    params.put(key, value);
+                }
+            }
+
+            if ("from".equals(tag) || tag.startsWith("from ")) {
+                //from标记
+                int i3 = tag.indexOf("<");
+                int i4 = tag.indexOf(">", i3);
+                if (i3 != -1 && i3 < i4) {
+                    requestContent = tag.substring(i3+1, i4);
+                }
+            }
+
+            //判断[]后是否有内容
+            if (content.length()-1 == i2) {
+                break;
+            }
+
+            content = content.substring(i2 + 1);
+            continue;
+
+        }
+        if (requestContent == null || "".equals(requestContent)) {
+            this.open();
+            return;
+        }
+        String url = ip + ":" + port + "/" + requestContent;
+        if (params.size() > 0) {
+            url += "?";
+            Set<String>keys = params.keySet();
+            for (String key : keys) {
+                String value = params.get(key);
+                url += key + "=" + value + "&";
+            }
+            //remove last "&"
+            url = url.substring(0, url.length()-1);
+        }
+        System.out.println(url);
+        this.parseURL(url);
     }
 
 
